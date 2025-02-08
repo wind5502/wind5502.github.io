611d9ec5a373b842800292125a88a194fde9a61b
wind5502 <145239284+wind5502@users.noreply.github.com>
2025-02-09 01:07:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-let audioBuffer;
-fetch("song.mp3")
-    .then(response => response.arrayBuffer())
-    .then(data => audioContext.decodeAudioData(data))
-    .then(buffer => {
-        audioBuffer = buffer;
-    });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const confirmBtn = document.querySelector('.confirm-btn');
     const body = document.body;
-    document.addEventListener("click", () => {
-    if (audioContext.state === "suspended") {
-        audioContext.resume();
-    }
-    if (audioBuffer) {
-        const source = audioContext.createBufferSource();
-        source.buffer = audioBuffer;
-        source.connect(audioContext.destination);
-        source.start();
-    }
-});
-
 
     confirmBtn.addEventListener('click', () => {
         // 添加切换类触发动画
         body.classList.add('show-content');
         
+    let video = document.getElementById("hiddenVideo");
+    video.muted = false; // 解除静音
+    video.play(); // 确保播放
         // 移除按钮点击事件
         confirmBtn.style.pointerEvents = 'none';
         
         // 可选：添加音效（需要准备音频文件）
         // const audio = new Audio('audio/click-sound.mp3');
-        // audio.play();
-    });
-})；
+        // audio.play()；
+});
+
